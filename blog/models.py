52ad48b067a7a758910932f6fa7a36ed757ed270
Ayushman Adhikari <unixparadoxfb@gmail.com>
2026-05-04 12:26:03 +0545

    adding db call to home view function

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Posts(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    posted_on = models.DateTimeField()
+    posted_on = models.DateTimeField(default=timezone.now, null=True)
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -2,38 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Posts as dbPosts
 
-posts = [
-    {
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'author': 'Ayusman',
-        'date_posted': 'June 1, 2024'
-    },
-    {
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'author': 'Ashesh',
-        'date_posted': 'June 15, 2025'
-    },
-    {
-        'title': 'Blog Post 3',
-        'content': 'Second post content',
-        'author': 'Ashesh',
-        'date_posted': 'June 15, 2025'
-    },
-    {
-        'title': 'Blog Post 4',
-        'content': 'Fourth post content',
-        'author': 'Ayusman',
-        'date_posted': 'June 20, 2025'
-    }
-
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': dbPosts.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
